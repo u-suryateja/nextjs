cf846a2da855bdd38f070e0929a4eab380c2ac91
underlineit-qa-2 <suryatejaupmaka4@gmail.com>
2026-03-02 22:54:53 +0530

    commit chages

--- app/pages/Dashboard.tsx
+++ app/pages/Dashboard.tsx
@@ -1,7 +1,11 @@
+"use client"
+import { useRouter } from "next/navigation"
 export default function Dashboard(){
+    const router=useRouter()
     return(
         <div className="flex justify-center items-center h-screen text-4xl">
-            Hello World...!
+           
+            <button onClick={()=>{router.push("/pages/displayProducts")}} className=" border-2 border-black bg-black text-white p-2 rounded-xl cursor-pointer">Products</button>
         </div>
     )
 }
